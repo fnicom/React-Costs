000e5061455a9518d8a73600fe92834b24f5114c
fnicom <fnicomedes@gmail.com>
2021-10-03 21:56:02 -0300

    first commit :tada:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+    <Link to="/">Home</Link>
+    <Link to="/contact">Contato</Link>
+    <Link to="/company">Company</Link>
+    <Link to="/newproject">Novo Projeto</Link>
+      </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/company">
+        <Company />
+      </Route>
+      <Route exact path="/contact">
+        <Contact />
+      </Route>
+      <Route exact path="/newproject">
+        <NewProject />
+      </Route>
+    </Switch>
+      <p>Footer</p>
+    </Router>
+  )
 }
 
-export default App;
+export default App
